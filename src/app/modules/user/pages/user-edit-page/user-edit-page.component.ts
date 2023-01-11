3e8ea299a4dd69e43dc6760a06d5c27874fed247
Veronika Litvin <VeronikaLitvin@coherentsolutions.com>
2023-01-11 12:31:51 +0300

    feat: add handling email

--- src/app/modules/user/pages/user-edit-page/user-edit-page.component.ts
+++ src/app/modules/user/pages/user-edit-page/user-edit-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { of, Subscription, switchMap } from 'rxjs';
+import { merge, of, Subscription, switchMap } from 'rxjs';
 import { IUser } from '../../models/user.interface';
 import { UserService } from '../../services/user.service';
 
@@ -17,7 +17,7 @@ export class UserEditPageComponent implements OnInit, OnDestroy {
 
   currentUser!: IUser;
 
-  subscription! : Subscription;
+  subscriptions: Subscription[] = [];
 
   constructor(private userService: UserService, private router: Router, private route: ActivatedRoute, private formBuilder: FormBuilder) { }
 
@@ -27,22 +27,42 @@ export class UserEditPageComponent implements OnInit, OnDestroy {
 
   get addresses(): FormArray {
     return this.editPageForm.get('addresses') as FormArray;
- } 
+  }
+
 
   addChildForm(form: FormGroup, key: string) {
     this.editPageForm.addControl(key, form);
 
-    this.subscription = this.route.paramMap.pipe(
+    const patchSubscription = this.route.paramMap.pipe(
       switchMap((params: ParamMap) =>
         of(params.get('id'))
       )
     ).subscribe(currentUserId => {
       this.currentUser = this.userService.getUsers().find(user => user.id === Number(currentUserId))!;
       this.editPageForm.get('user')!.patchValue(this.currentUser);
-      if(this.addresses) {
+      if (this.addresses) {
         this.addresses.patchValue(this.currentUser.addresses);
       }
     });
+
+    this.subscriptions.push(patchSubscription);
+
+    this.checkValueChanges();
+  }
+
+  checkValueChanges() {
+    const firstNameControl = this.editPageForm.controls['user'].get('firstName');
+    const lastNameControl = this.editPageForm.controls['user'].get('lastName');
+
+    const valueChangesSubscription = merge(
+      firstNameControl!.valueChanges,
+      lastNameControl!.valueChanges
+    ).subscribe(() => {
+      const value = `${firstNameControl!.value.toLowerCase()}.${lastNameControl!.value.toLowerCase()}@gmail.com`
+      this.editPageForm.get('user')!.get('userEmail')!.setValue(value);
+    })
+
+    this.subscriptions.push(valueChangesSubscription);
   }
 
   editUser() {
@@ -55,8 +75,8 @@ export class UserEditPageComponent implements OnInit, OnDestroy {
     }
   }
 
-  ngOnDestroy(){
-    this.subscription.unsubscribe();
+  ngOnDestroy() {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 
 }
